8f65fe58fcc8d48d61a2cd06538632974fe246a8
H4rfu1 <mohfahrulhafidh@gmail.com>
2020-09-05 08:38:36 +0700

    flash audio

--- scene/splash.gd
+++ scene/splash.gd
@@ -6,10 +6,13 @@ func _ready():
 	$tape.show()
 	yield(get_tree().create_timer(0.5), "timeout")
 	$AnimationPlayer.play("tape_open")
+#	$flash.play()
+	$flash2.play()
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if(anim_name == "tape_open"):
 		$tape.hide()
 		$AnimationPlayer.play("game_logo")
+		$wave.play()
 	else:
 		get_tree().change_scene("res://scene/Menu/Main.tscn")
